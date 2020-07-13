c5485b247fe1b17a08da7345749d06321a7d9f88
gutianci <gutianci@pwrd.com>
2020-07-13 12:07:25 +0800

    feat: edit tag dialog ui

--- src/Tags.js
+++ src/Tags.js
@@ -3,6 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import Chip from '@material-ui/core/Chip';
 import Dialog from '@material-ui/core/Dialog';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import MuiDialogActions from '@material-ui/core/DialogActions';
 
 import DialogTitleWithCloseIcon from './DialogTitleWithCloseIcon';
 import DialogContent from './DialogContent';
@@ -39,27 +41,51 @@ const useStyles = makeStyles((theme) => ({
 /**
  * props:
  *   tags: [array[str]]
+ *   onTagEdit [callback]
+ *   onTagDelete [callback]
  */
 export default function Tags({
   tags,
+  onTagEdit,
+  onTagDelete,
 }) {
   const classes = useStyles();
 
-  const [operateDialogOpen, setOperateDialogOpen] = useState(false);
+  // chosen tag
+  const [tag, setTag] = useState({
+    index: -1,
+    content: '',
+  });
 
-  const handleOperateDialogOpen = () => {
-    setOperateDialogOpen(true);
+  const handleChooseTag = (index, content) => {
+    setTag({
+      index,
+      content,
+    });
   }
 
-  const handleOperateDialogClose = () => {
-    setOperateDialogOpen(false);
+  const resetTag = () => {
+    setTag({
+      index: -1,
+      content: '',
+    });
+  }
+
+  const [editDialogOpen, setEditDialogOpen] = useState(false);
+
+  const handleOpenEditDialog = () => {
+    setEditDialogOpen(true);
+  }
+
+  const handleCloseEditDialog = () => {
+    setEditDialogOpen(false);
   }
 
   return (
     <div className={classes.root}>
       {tags.map((t, i) => {
         return (
-          <Chip key={i} label={t} onClick={handleOperateDialogOpen} />
+          <Chip key={i} label={t} onClick={() => handleChooseTag(i, t)} />
         )})}
       <Dialog
         fullWidth
@@ -68,21 +94,40 @@ export default function Tags({
           paperFullWidth: classes.paperFullWidth,
           scrollPaper: classes.scrollPaper,
         }}
-        open={operateDialogOpen}
-        onClose={handleOperateDialogClose}
-        aria-labelledby="delete-tag-confirm-dialog"
+        open={!!tag.content}
+        onClose={resetTag}
+        aria-labelledby="choose-tag-operation"
       >
-        <DialogTitleWithCloseIcon id="delete-tag-confirm-dialog" onClose={handleOperateDialogClose}>
+        <DialogTitleWithCloseIcon id="choose-tag-operation" onClose={resetTag}>
           请选择操作
         </DialogTitleWithCloseIcon>
         <DialogContent dividers className={classes.operateDialog}>
-          <Button fullWidth variant="contained" color="primary" onClick={() => console.log('handle edit tag')}>
+          <Button fullWidth variant="contained" color="primary" onClick={handleOpenEditDialog}>
             编辑标签
           </Button>
-          <Button fullWidth variant="contained" color="primary" onClick={() => console.log('handle delete tag')}>
+          <Button fullWidth variant="contained" color="primary" onClick={() => console.log(`handle delete tag ${tag.content}`)}>
             删除标签
           </Button>
         </DialogContent>
+      </Dialog>
+      <Dialog
+        fullWidth
+        maxWidth="sm"
+        open={editDialogOpen}
+        onClose={handleCloseEditDialog}
+        aria-labelledby="edit-tag"
+      >
+        <DialogTitleWithCloseIcon id="edit-tag" onClose={handleCloseEditDialog}>
+          编辑标签
+        </DialogTitleWithCloseIcon>
+        <DialogContent dividers>
+          <form noValidate autoComplete="off">
+            <TextField fullWidth required defaultValue={tag.content} id="标签" label="标签" margin="normal" />
+          </form>
+        </DialogContent>
+        <MuiDialogActions>
+          <Button variant="contained" color="primary" onClick={() => console.log('handle submit edit tag dialog')}>确认</Button>
+        </MuiDialogActions>
       </Dialog>
     </div>
   );
