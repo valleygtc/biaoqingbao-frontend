f9d4a62002bea0ad0a291ad04b58b85e74772359
gutianci <gutianci@pwrd.com>
2020-07-13 14:00:25 +0800

    feat: pagination ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Divider from '@material-ui/core/Divider';
+import Pagination from '@material-ui/lab/Pagination';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 import Header from './Header';
 import ImageWall from './ImageWall';
@@ -15,17 +17,32 @@ const useStyles = makeStyles((theme) => ({
   },
   content: {
     flexGrow: '1',
+  },
+  pagination: {
+    display: 'flex',
+    justifyContent: 'center',
+    margin: theme.spacing(2, 0),
   }
 }));
 
 export default function App({}) {
   const classes = useStyles();
+  const theme = useTheme();
+  const bigScreen = useMediaQuery(theme.breakpoints.up('sm'));
+  const [page, setPage] = React.useState(1);
+  const handleChange = (event, value) => {
+    setPage(value);
+    console.log(`change to page: ${value}`)
+  };
 
   return (
     <div className={classes.root}>
       <Header />
       <div className={classes.content}>
         <ImageWall />
+        <div className={classes.pagination}>
+          <Pagination count={10} size={bigScreen ? 'large' : 'medium'} page={page} onChange={handleChange} />
+        </div>
       </div>
       <Divider />
       <Footer />
