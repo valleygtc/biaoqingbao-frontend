25cac763b6c6f6469a664c208ee5c75e49cd8537
gutianci <gutianci@pwrd.com>
2020-07-13 14:03:42 +0800

    feat: footer ui center copyright text

--- src/Footer.js
+++ src/Footer.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '10vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+}));
+
 export default function Footer() {
+  const classes = useStyles();
+
   return (
-    <div
-      style={{
-        height: '10vh',
-        textAlign: 'center',
-      }}
-    >
+    <div className={classes.root}>
       <Typography variant="body2" noWrap>
         &copy; 2020 gutianci
       </Typography>
