dff76599494d39a40f5992e0929987abdb79e122
gutianci <gutianci@pwrd.com>
2020-07-21 11:29:31 +0800

    feat: add image add group select

--- src/AddImageDialog.js
+++ src/AddImageDialog.js
@@ -8,18 +8,26 @@ import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 
 import DialogTitleWithCloseIcon from './DialogTitleWithCloseIcon';
 import DialogContent from './DialogContent';
 import { addImage } from './mainSlice';
 
 function AddImageDialog({
+  groups,
   open,
   onClose,
   addImage,
 }) {
   const theme = useTheme();
   const dialogFullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const [group, setGroup] = React.useState(String(groups[0].id));
+  const handleChange = (event) => {
+    setGroup(event.target.value);
+  };
 
   return (
     <Dialog
@@ -34,10 +42,23 @@ function AddImageDialog({
         添加图片
       </DialogTitleWithCloseIcon>
       <DialogContent dividers>
-        <form noValidate autoComplete="off">
+        <form noValidate>
           <FormControl required fullWidth margin="normal">
             <InputLabel shrink htmlFor="image-picker">图片</InputLabel>
             <Input id="image-picker" type="file" onChange={() => console.log('handle file choose')} />
+          </FormControl>
+          <FormControl required fullWidth margin="normal">
+            <InputLabel id="group">组</InputLabel>
+            <Select
+              labelId="group"
+              id="group"
+              value={group}
+              onChange={handleChange}
+            >
+              {groups.map((g) => (
+                <MenuItem key={g.id} value={String(g.id)}>{g.name}</MenuItem>
+              ))}
+            </Select>
           </FormControl>
           <TextField id="标签" label="标签" fullWidth margin="normal"/>
           <FormControl margin="normal">
@@ -49,9 +70,13 @@ function AddImageDialog({
   );
 }
 
+const mapStateToProps = (state) => ({
+  groups: state.main.groups,
+});
+
 const mapDispatchToProps = { addImage };
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(AddImageDialog);
