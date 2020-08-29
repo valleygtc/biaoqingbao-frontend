06ef16a8fb8a09ee81f376b066b408dde87bc278
gutianci <gutianci@pwrd.com>
2020-08-29 18:37:52 +0800

    feat: cache image resp with service worker.

---
+++ src/sw.js
@@ -0,0 +1,17 @@
+// cache local assets
+workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+
+// cache image
+workbox.routing.registerRoute(
+  new RegExp('/api/images/.+'),
+  new workbox.strategies.CacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200],
+      }),
+    ],
+  }),
+);
+
+// TODO: cache getImageList, getGroups, NetworkFirst
